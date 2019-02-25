0b6aa7f745932cc5908ea0ee83becf8b15c0a7e6
MarkLozier <lozierm2@gmail.com>
2019-02-25 15:11:06 -0500

    Index Veiw

---
+++ Resturantreader.DotWebMVC/Controllers/ResturantController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resturantreader.DotWebMVC.Controllers
+{
+    public class ResturantController : Controller
+    {
+        // GET: Resturant
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
