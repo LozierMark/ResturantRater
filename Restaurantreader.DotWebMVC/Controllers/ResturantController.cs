faf3c7f46ee8f319badfc22e45646e80dde0bf1f
MarkLozier <lozierm2@gmail.com>
2019-02-26 13:12:48 -0500

    backupTacoTuesday

--- Restaurantreader.DotWebMVC/Controllers/ResturantController.cs
+++ Restaurantreader.DotWebMVC/Controllers/ResturantController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Restaurantreader.DotWebMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using static Restaurantreader.DotWebMVC.Models.Restaurant;
@@ -20,5 +22,64 @@ namespace Restaurantreader.DotWebMVC.Controllers
         {
             return View();
         }
+        //Post: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RestaurantID, Name, Address, Rating")]Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurants.Add(restaurant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = db.Restaurants.Find(id);
+
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(restaurant);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Restaurant restaurant = db.Restaurants.Find(id);
+            db.Restaurants.Remove(restaurant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+                {
+                    base.Dispose(disposing);
+
+                }
+            }
+        }
+
+
     }
 }
+
+
+
+
+
+
+
+
